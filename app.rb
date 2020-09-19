21d7808d4f5303b70298df3664538f33bf85fe13
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-19 03:33:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,13 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-    get '/' do
+  get '/' do
       erb :index
-      "<h1>Hello World</h1>"
     end
  
     get "/info" do
-      "Testing the info page"
+      erb :info
     end
 
 	 
